29f8fb2f6d9493d5177cde191d8e0e13a4afe45e
Cubicheng <2630377728@qq.com>
2024-08-20 20:25:51 +0800

    finish 1.4.textures

---
+++ src/1.4.textures/box_texture/shaders.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "glad/glad.h"
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+
+class Shader {
+public:
+	unsigned int programID;
+
+	Shader(const char* vertexPath, const char* fragmentPath);
+
+	void checkShaderCompile(const unsigned int& shader,const char* shader_name);
+	void checkProgramLink(const unsigned int& program);
+
+	void use();
+	void deleteProgram();
+
+	void setUniformBool(const std::string& name, bool value)const;
+	void setUniformInt(const std::string& name, int value)const;
+	void setUniformFloat(const std::string& name, float value)const;
+	void setUniform4f(const std::string& name, float x, float y, float z, float w)const;
+};
